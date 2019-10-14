ae6ed0ed67806bbb0cf7d63fa1f34f3f0ee4d338
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-14 00:29:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+# 1.convert hash to array 
+# 2.find the minimum value
+# 3.Compare minimum value to has values
+# 4. Find the minimum hash value 
+# 5. Print the key with that hash value 
+'require pry'
 
-def key_for_min_value(name_hash)
-
-end
